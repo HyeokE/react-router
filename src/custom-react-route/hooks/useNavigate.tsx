5e23de96125c04546ddd0a602b02806302ba37bf
HyeokE <jhjeong00@gmail.com>
2022-10-06 19:27:50 +0900

    chore: modify navigate function

--- src/custom-react-route/hooks/useNavigate.tsx
+++ src/custom-react-route/hooks/useNavigate.tsx
@@ -4,15 +4,11 @@ import { RouteContext } from '../contexts/RouteContext';
 export const useNavigate = () => {
   let { path, setPath } = useContext(RouteContext);
 
-  const navigate = useCallback(
-    (inputPath: string) => {
-      if (path.pathname === inputPath) return;
-      history.pushState({}, '', inputPath);
-      const newObj = Object.assign({}, location);
-
-      setPath(newObj);
-    },
-    [path],
-  );
+  const navigate = useCallback((inputPath: string) => {
+    if (path.pathname === inputPath) return;
+    history.pushState({}, '', inputPath);
+    const newObj = Object.assign({}, location);
+    setPath(newObj);
+  }, []);
   return navigate;
 };
